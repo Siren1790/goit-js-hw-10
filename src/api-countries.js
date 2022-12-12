c6215813b71aae2970c4471f1f251d18c725d1e8
Sergii Karpus <karpus.sergey@gmail.com>
2022-12-12 22:50:42 +0200

    in Work Task 10

---
+++ src/api-countries.js
@@ -0,0 +1,6 @@
+function searchCountries(value) {
+   return fetch(`https://restcountries.com/v3.1/name/${value}`)
+            .then(response => response.json())
+}
+
+export default { searchCountries};
--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+
+import API from "./api-countries"
+
+refs = {
+   searchBox: document.querySelector("#search-box"),
+   countryList: document.querySelector('.country-list'),
+   countryInfo: document.querySelector('.country-info'),
+}
+
+refs.searchBox.addEventListener("keyup", searchCountries);
+
+function searchCountries (e){
+
+   API.searchCountries(e.target.value)
+      .then(data => {
+      if (data.length > 10){
+         console.log("Потрібно більше букв")
+      } else {
+         for(info of data){
+            murkupCountries(info)            
+         }
+      }
+   })
+   .catch(error => {
+      console.log(error);
+   })
+}
+
+function murkupCountries ({name: {official}, capital, population, flags: {svg}, languages}){
+   console.log(`official: ${official}, capital: ${capital}, population: ${population}, svg: ${svg}, languages: ${Object.values(languages)}`);
+}
